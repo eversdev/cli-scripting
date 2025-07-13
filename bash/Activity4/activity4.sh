d93176ff44f780c91e2b8d6d7422e031ddb07087
eversdev <beversprof25@outlook.com>
2025-07-14 00:49:33 +0100

    Add password validiation with lenght and letter case and number checks - Activity 4

--- bash/Activity4/activity4.sh
+++ bash/Activity4/activity4.sh
@@ -13,9 +13,19 @@ if [ ${#password} -lt 8 ]; then
 	echo "Password requires minium of 8 chars or more!"
 	exit 1
 
+elif  [[ ! "$password" =~ [A-Z] || ! "$password" =~ [a-z] ]]; then
+       echo "Password must contain both uppercase and lowercase letters."
+       exit 1
+
 elif [[ "$password" =~ [A-Za-z] && "$password" =~ [0-9] ]]; then
        echo "Password is valid."
        exit 0
+       
+else
+    echo "Password must include at least one letter and one number!"
+    exit 1
 
 fi
  
+
+
