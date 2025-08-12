0e02bc2f0ae93977e7a062f334a3c3fb47f53777
eversdev <beversprof25@outlook.com>
2025-08-13 00:59:44 +0100

    Complete Activity 1: user session info script in Powershell

--- powershell/activity1.ps1
+++ powershell/activity1.ps1
@@ -1,3 +1,11 @@
-Write-Host "your username is $env:USER"
+Write-Host "your username is $env:USERNAME"
 
+Write-Host "Todays date is $(Get-Date -Format 'dd:MM:yyyy')"
 
+Write-Host "The time is $(Get-Date -Format 'hh:mm:ss')"
+
+Write-Host "Your current directory is $(Get-Location)" 
+
+Write-Host "Number of current files in this directory is $((Get-ChildItem -File).Count)"
+
+Write-Host "The biggest file in this directory is $((Get-ChildItem -File | Sort-Object -Property Length -Descending | Select-Object -First 1).Name)"
